054d0ea415f486cbe54379ff50232d0d980020fb
Cristian Martinez <dbautista2681@gmail.com>
2021-09-07 15:55:42 -0500

    Commit changes

---
+++ src/main/java/com/senecafoundation/webpokedexgame/WebPokemonGameApplication.java
@@ -0,0 +1,13 @@
+package com.senecafoundation.webpokedexgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPokemonGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPokemonGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senecafoundation/webpokedexgame/WebPokemonGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senecafoundation.webpokedexgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPokemonGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
