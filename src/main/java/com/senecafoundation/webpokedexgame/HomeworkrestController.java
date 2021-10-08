7b839fbd8e0377a74c570a487a61920d40a71701
Rewpi <83379309+Rewpi@users.noreply.github.com>
2021-10-07 22:29:42 -0500

    Homework Rest Controller

---
+++ src/main/java/com/senecafoundation/webpokedexgame/HomeworkrestController.java
@@ -0,0 +1,24 @@
+package com.senecafoundation.webpokedexgame;
+
+import com.senecafoundation.webpokedexgame.DataHandler.HomeworkDataWriter;
+import com.senecafoundation.webpokedexgame.PokedexItems.Homework;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeworkrestController {
+    
+    @Autowired 
+    @Qualifier("HomeworkDataWriter")
+    HomeworkDataWriter dataHandler;
+
+    @PostMapping("/Homeworks")
+        Homework newHomework(@RequestBody Homework newHomework) {
+        dataHandler.Create(newHomework);
+        return newHomework;
+    }
+}
