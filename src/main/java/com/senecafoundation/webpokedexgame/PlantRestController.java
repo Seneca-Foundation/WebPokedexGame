bdf3ef08083a108d1c1ea0714f67225f5d5e72cf
Cristian Martinez <dbautista2681@gmail.com>
2021-10-11 16:35:33 -0500

    I don't get it RestController is too confusing

--- src/main/java/com/senecafoundation/webpokedexgame/PlantRestController.java
+++ src/main/java/com/senecafoundation/webpokedexgame/PlantRestController.java
@@ -1,14 +1,18 @@
 package com.senecafoundation.webpokedexgame;
+import java.util.List;
 import java.util.UUID;
 
 import com.senecafoundation.webpokedexgame.DataHandler.PlantDataWriter;
 import com.senecafoundation.webpokedexgame.PokedexItems.Plant;
+import com.senecafoundation.webpokedexgame.PokedexItems.PokedexItem;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,11 +25,11 @@ public class PlantRestController {
     
     PlantDataWriter dataHandler;
     
-    @PostMapping("/plants")
-        Plant newPlant(@RequestBody Plant newPlant) {
-        dataHandler.Create(newPlant);
-        return newPlant;
-    }
+    // @PostMapping("/plants")
+    //     Plant newPlant(@RequestBody Plant newPlant) {
+    //     dataHandler.Create(newPlant);
+    //     return newPlant;
+    // }
 
 
     // @GetMapping("/plants/{id}")
@@ -35,6 +39,10 @@ public class PlantRestController {
     //     .orElseThrow(() -> newPlantNotFoundException(id));
     // }
 
+    @GetMapping("/plants")
+    List<PokedexItem> plantList = dataHandler.ReadAll()
+
+
     @GetMapping("/plants/{id}")
     Plant plant(@PathVariable Long id) {
 
@@ -42,10 +50,14 @@ public class PlantRestController {
         .orElseThrow(() -> new Exception(id));
     }
 
+    @PutMapping("/plants/{id}")
+    Plant replacePlant(@RequestBody Plant  )
+
 
     @DeleteMapping("/plants/{id}")
     void deleteplant(@PathVariable String id) {
         PlantDataWriter.Delete(UUID.fromString(id));
     }
 
+    
 }
