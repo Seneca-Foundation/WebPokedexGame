f365c23737c8cafea06e2b8a94d4d321714d56a4
wtvamp <warrenjthompson@gmail.com>
2021-10-21 13:44:42 -0700

    Adding get by id

--- src/main/java/com/senecafoundation/webpokedexgame/PokemonSecondRestController.java
+++ src/main/java/com/senecafoundation/webpokedexgame/PokemonSecondRestController.java
@@ -6,7 +6,6 @@ import java.util.UUID;
 import com.senecafoundation.webpokedexgame.DataHandler.AnimatedPropertiesDataWriter;
 import com.senecafoundation.webpokedexgame.DataHandler.PokemonSecondDataWriter;
 import com.senecafoundation.webpokedexgame.PokedexItems.AnimatedProperties;
-import com.senecafoundation.webpokedexgame.PokedexItems.PokedexItem;
 import com.senecafoundation.webpokedexgame.PokedexItems.Pokemon.PokemonWithSecondAbility;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -43,6 +41,13 @@ public class PokemonSecondRestController {
     @GetMapping("/pokemonSeconds")
     List<PokemonWithSecondAbility> allPokemonSeconds() {
         return dataHandler.ReadAll();
+    }
+
+
+    @GetMapping("/pokemonSeconds/{id}")
+    PokemonWithSecondAbility getPokemonWithId(@PathVariable String id) throws Exception
+    {
+        return dataHandler.Read(UUID.fromString(id));
     }
     
     @PutMapping("/pokemonSeconds/{id}")
