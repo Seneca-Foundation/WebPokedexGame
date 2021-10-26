287fb4d789bbd14233e38e67e37bfff813f06b01
Cristian Martinez <dbautista2681@gmail.com>
2021-10-26 14:29:45 -0500

    Added DataWriter and Repo To Trainer

---
+++ src/main/java/com/senecafoundation/webpokedexgame/Repositories/BaseCharacterRepositories/TrainerCharacterRepository.java
@@ -0,0 +1,10 @@
+package com.senecafoundation.webpokedexgame.Repositories.BaseCharacterRepositories;
+
+import com.senecafoundation.webpokedexgame.Trainers.Trainer;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrainerCharacterRepository<T extends Trainer> extends BaseCharacterRepository<T>  {
+    
+}
