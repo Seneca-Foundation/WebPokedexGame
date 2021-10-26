448667e13d460cdb53284935229986852eb5e85d
Cristian Martinez <dbautista2681@gmail.com>
2021-10-25 20:32:54 -0500

    Added Entity and Inhertience to Trainer Object

--- src/main/java/com/senecafoundation/webpokedexgame/Trainers/Trainer.java
+++ src/main/java/com/senecafoundation/webpokedexgame/Trainers/Trainer.java
@@ -4,9 +4,15 @@ import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+
 import com.senecafoundation.webpokedexgame.PokedexItems.AnimatedProperties;
 import com.senecafoundation.webpokedexgame.PokedexItems.Pokemon.Pokemon;
 
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 public class Trainer extends NPC {
 
     private List<Pokemon> sixPokemon; 
