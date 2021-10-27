a0b37e3c234a27b9211d50cef55dbc96cf04f775
WTVamp <warrenjthompson@gmail.com>
2021-10-27 15:50:55 -0700

    Finished up the trainer object
    Co-authored-by: Cristian Martinez <dbautista2681@gmail.com>
    Co-authored-by: Rewpi <Rewpi@users.noreply.github.com>

--- src/main/java/com/senecafoundation/webpokedexgame/Trainers/Trainer.java
+++ src/main/java/com/senecafoundation/webpokedexgame/Trainers/Trainer.java
@@ -1,47 +1,56 @@
-// package com.senecafoundation.webpokedexgame.Trainers;
+package com.senecafoundation.webpokedexgame.Trainers;
 
-// import java.util.ArrayList;
-// import java.util.Hashtable;
-// import java.util.List;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
 
-// import javax.persistence.Entity;
-// import javax.persistence.Inheritance;
-// import javax.persistence.InheritanceType;
+import javax.persistence.CascadeType;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
 
-// import com.senecafoundation.webpokedexgame.PokedexItems.AnimatedProperties;
-// import com.senecafoundation.webpokedexgame.PokedexItems.Pokemon.Pokemon;
+import com.senecafoundation.webpokedexgame.PokedexItems.AnimatedProperties;
+import com.senecafoundation.webpokedexgame.PokedexItems.Pokemon.Pokemon;
+import com.senecafoundation.webpokedexgame.PokedexItems.Pokemon.PokemonWithSecondAbility;
 
-// @Entity
-// @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-// public class Trainer extends NPC {
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class Trainer extends NPC {
 
-//     private List<Pokemon> sixPokemon; 
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval=true)
+    private List<PokemonWithSecondAbility> sixPokemon; 
 
-//     private Integer trainerLevel; 
+    private Integer trainerLevel; 
 
-//     public Trainer(List<Pokemon> sixPokemon, Integer trainerLevel, String name, AnimatedProperties animatedProperties, Hashtable<String, ArrayList<String>> dialogueTree ) {
-//         super(name, animatedProperties, dialogueTree);
-//         this.setSixPokemon(sixPokemon);
-//         this.setTrainerLevel(trainerLevel);
-//     }
+    public Trainer() {
+        super();
+    }
 
-//     public List<Pokemon> getSixPokemon() {
-//         return sixPokemon;
-//     }
+    public Trainer(ArrayList<PokemonWithSecondAbility> sixPokemon, Integer trainerLevel, String name, AnimatedProperties animatedProperties, Hashtable<String, ArrayList<String>> dialogueTree ) {
+        super(name, animatedProperties, dialogueTree);
+        this.setSixPokemon(sixPokemon);
+        this.setTrainerLevel(trainerLevel);
+    }
 
-//     public void setSixPokemon(List<Pokemon> sixPokemon)  {
-//         this.sixPokemon = sixPokemon; 
-//     }
+    public List<PokemonWithSecondAbility> getSixPokemon() {
+        return sixPokemon;
+    }
 
-//     public Integer getTrainerLevel() {
-//         return trainerLevel;
-//     }
+    public void setSixPokemon(List<PokemonWithSecondAbility> sixPokemon)  {
+        this.sixPokemon = sixPokemon; 
+    }
+
+    public Integer getTrainerLevel() {
+        return trainerLevel;
+    }
     
-//     public void setTrainerLevel(Integer trainerLevel) {
-//         this.trainerLevel = trainerLevel; 
-//     }
+    public void setTrainerLevel(Integer trainerLevel) {
+        this.trainerLevel = trainerLevel; 
+    }
 
 
 
     
-// }
+}
