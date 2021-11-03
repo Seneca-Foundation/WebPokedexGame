58951d936aedb3ba9797f89cc95985ce14e6cddb
Cristian Martinez <dbautista2681@gmail.com>
2021-11-03 18:53:47 -0500

    Lots of tiles left but finally fixed the look and width

--- src/main/resources/static/js/maps/WaterFallMap.js
+++ src/main/resources/static/js/maps/WaterFallMap.js
@@ -1,5 +1,37 @@
-waterFallMap = new MapPath(false, 8000, "/images/maps/waterfallmap.png", '100%', 25, 30, 205, 220.5);
+waterFallMap = new MapPath(false, 8300, "/images/maps/waterfallmap.png", '100%', 30, 17.55, 205, 220.5);
 waterFallMap.setUpPaths = function() {
+
+    // waterFallMap.switchBlockedRangeVert(3355, 11, 83);
+    // waterFallMap.switchBlockedRangeVert(3946, 4, 83);
+    // waterFallMap.switchBlockedRangeVert(3462, 23, 83);
+    // waterFallMap.switchBlockedRangeVert(5171, 9, 83);
+    // waterFallMap.switchBlockedRangeVert(6008, 2, 83);
+    // waterFallMap.switchBlockedRangeVert(4352, 9, 83);
+    // waterFallMap.switchBlockedRangeVert(4859, 5, 83);
+    // waterFallMap.switchBlockedRangeVert(5931, 1, 83);
+    // waterFallMap.switchBlockedRangeVert(3721, 10, 83);
+    // waterFallMap.switchBlockedRangeVert(6019, 1, 83);
+    waterFallMap.switchBlockedRangeVert(3447, 10, 100);
+    waterFallMap.switchBlockedRangeVert(4446, 5, 100);
+    waterFallMap.switchBlockedRangeVert(3556, 16, 100);
+    
+    
+    // waterFallMap.switchBlockedRange(3366, 3379);
+    // waterFallMap.switchBlockedRange(5275, 5287);
+    // waterFallMap.switchBlockedRange(3338, 3354);
+    // waterFallMap.switchBlockedRange(5095, 5098);
+    // waterFallMap.switchBlockedRange(5919, 5925);
+    // waterFallMap.switchBlockedRange(4860, 4872);
+    // waterFallMap.switchBlockedRange(6092, 6097);
+    // waterFallMap.switchBlockedRange(5932, 5936);
+    // waterFallMap.switchBlockedRange(3629, 3638);
+    // waterFallMap.switchBlockedRange(4542, 4550);
+    // waterFallMap.switchBlockedRange(3388, 3400);
+    // waterFallMap.switchBlockedRange(6186, 6224);
+    waterFallMap.switchBlockedRange(5036, 5047);
+    waterFallMap.switchBlockedRange(3448, 3456);
+    
+
 
 
 }
