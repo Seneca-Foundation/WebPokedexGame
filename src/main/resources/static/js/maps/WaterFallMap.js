941601caead6d0a1a4db1051bb3f6b912312fc52
Cristian Martinez <dbautista2681@gmail.com>
2021-11-03 19:11:38 -0500

    Added more tiles will brb in 15 min

--- src/main/resources/static/js/maps/WaterFallMap.js
+++ src/main/resources/static/js/maps/WaterFallMap.js
@@ -1,6 +1,9 @@
 waterFallMap = new MapPath(false, 8300, "/images/maps/waterfallmap.png", '100%', 30, 17.55, 205, 220.5);
 waterFallMap.setUpPaths = function() {
 
+
+    waterFallMap.switchBlocked(4127)
+
     // waterFallMap.switchBlockedRangeVert(3355, 11, 83);
     // waterFallMap.switchBlockedRangeVert(3946, 4, 83);
     // waterFallMap.switchBlockedRangeVert(3462, 23, 83);
@@ -14,6 +17,8 @@ waterFallMap.setUpPaths = function() {
     waterFallMap.switchBlockedRangeVert(3447, 10, 100);
     waterFallMap.switchBlockedRangeVert(4446, 5, 100);
     waterFallMap.switchBlockedRangeVert(3556, 16, 100);
+    waterFallMap.switchBlockedRangeVert(4128, 10, 100);
+    waterFallMap.switchBlockedRangeVert(3426, 7, 100);
     
     
     // waterFallMap.switchBlockedRange(3366, 3379);
@@ -30,6 +35,15 @@ waterFallMap.setUpPaths = function() {
     // waterFallMap.switchBlockedRange(6186, 6224);
     waterFallMap.switchBlockedRange(5036, 5047);
     waterFallMap.switchBlockedRange(3448, 3456);
+    waterFallMap.switchBlockedRange(3255, 3264);
+    waterFallMap.switchBlockedRange(5159, 5163);
+    waterFallMap.switchBlockedRange(3236, 3247);
+    waterFallMap.switchBlockedRange(3271, 3280);
+    waterFallMap.switchBlockedRange(2417, 2427);
+    waterFallMap.switchBlockedRange(2434, 2443);
+    waterFallMap.switchBlockedRange(3215, 3228);
+    waterFallMap.switchBlockedRange(2405, 2409);
+    
     
 
 
