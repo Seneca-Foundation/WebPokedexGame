5ade6d25b90d15c3a30932be69206447b8cf33be
lunarjuice <mcaldwell@senecafoundation.org>
2021-11-04 15:44:59 -0500

    Creating HouseTwoMap and adding walls

---
+++ src/main/resources/static/js/maps/HouseTwoMap.js
@@ -0,0 +1,89 @@
+HouseTwoMap = new MapPath(false, 4233, "/images/maps/houseTwo.png", '100%', 25, 15.4, 21, 101);
+HouseTwoMap.setUpPaths = function() {
+
+    // -walls-
+HouseTwoMap.switchBlockedRangeVert(1747, 22, 83)
+HouseTwoMap.switchBlockedRangeVert (3571, 6, 83)
+HouseTwoMap.switchBlockedRangeVert(3676, 5, 83)
+HouseTwoMap.switchBlockedRangeVert(3600, 1, 83)
+HouseTwoMap.switchBlockedRangeVert(1519, 3, 83)
+HouseTwoMap.switchBlockedRangeVert(1359, 4, 83)
+HouseTwoMap.switchBlockedRangeVert(251, 14, 83)
+HouseTwoMap.switchBlockedRangeVert(1362, 2, 83)
+HouseTwoMap.switchBlockedRangeVert(280, 2, 83)
+HouseTwoMap.switchBlockedRangeVert(283, 2, 83)
+HouseTwoMap.switchBlockedRangeVert(612, 5, 83)
+HouseTwoMap.switchBlockedRangeVert(616, 5, 83)
+HouseTwoMap.switchBlockedRangeVert(1876, 16, 83)
+HouseTwoMap.switchBlockedRangeVert(1879, 16, 83)
+HouseTwoMap.switchBlockedRangeVert(3539, 7, 83)
+HouseTwoMap.switchBlockedRangeVert(315, 15, 83)
+HouseTwoMap.switchBlockedRangeVert(1906, 26, 83)
+HouseTwoMap.switchBlockedRangeVert(3911, 1, 83)
+HouseTwoMap.switchBlockedRangeVert(3919, 1, 83)
+HouseTwoMap.switchBlockedRangeVert(3262, 3, 83)
+HouseTwoMap.switchBlockedRangeVert(2266, 7, 83)
+HouseTwoMap.switchBlockedRangeVert(2273, 5, 83)
+HouseTwoMap.switchBlockedRangeVert(2371, 6, 83)
+
+HouseTwoMap.switchBlockedRangeVert(2455, 5, 83)
+HouseTwoMap.switchBlockedRangeVert(2456, 5, 83)
+
+
+
+
+
+HouseTwoMap.switchBlockedRange(3185, 3186)
+HouseTwoMap.switchBlockedRange(2360, 2368)
+
+
+HouseTwoMap.switchBlockedRange(2372, 2373)
+
+
+HouseTwoMap.switchBlockedRange(3268, 3274)
+HouseTwoMap.switchBlockedRange(3197, 3203)
+HouseTwoMap.switchBlockedRange(3280, 3286)
+HouseTwoMap.switchBlockedRange(2443, 2451)
+HouseTwoMap.switchBlockedRange(3178, 3179)
+HouseTwoMap.switchBlockedRange(3912, 3918)
+HouseTwoMap.switchBlockedRange(3995, 4001)
+HouseTwoMap.switchBlockedRange(3987, 3988)
+HouseTwoMap.switchBlockedRange(2495, 2513)
+HouseTwoMap.switchBlockedRange(3580, 3587)
+HouseTwoMap.switchBlockedRange(4070, 4090)
+HouseTwoMap.switchBlockedRange(1414, 1436)
+HouseTwoMap.switchBlockedRange(168, 197)
+HouseTwoMap.switchBlockedRange(529, 533)
+HouseTwoMap.switchBlockedRange(200, 232)
+HouseTwoMap.switchBlockedRange(1611, 1643)
+HouseTwoMap.switchBlockedRange(1276, 1280)
+HouseTwoMap.switchBlockedRange(1774, 1793)
+HouseTwoMap.switchBlockedRange(1796, 1823)
+HouseTwoMap.switchBlockedRange(4121, 4147)
+HouseTwoMap.switchBlockedRange(3453, 3456)
+HouseTwoMap.switchBlockedRange(3602, 3619)
+HouseTwoMap.switchBlockedRange(3593, 3596)
+HouseTwoMap.switchBlockedRange(3762, 3766)
+
+HouseTwoMap.switchBlockedRange(2523, 2525)
+
+
+HouseTwoMap.switchBlocked(3572)
+HouseTwoMap.switchBlocked(3679)
+
+
+//---------------------------------------------------------
+    // -Table & Chairs-
+HouseTwoMap.switchBlockedRangeVert (2915, 10, 83)
+HouseTwoMap.switchBlockedRangeVert (2924, 10, 83)
+HouseTwoMap.switchBlockedRange(3580, 3587)
+HouseTwoMap.switchBlockedRange(2835, 2838)
+HouseTwoMap.switchBlockedRange(2916, 2917)
+HouseTwoMap.switchBlockedRange(2922, 2923)
+
+
+//---------------------------------------------------------
+
+
+
+}
