e880342071551d9b3ce75a8fd5c08d4a4a6cb11f
Cristian Martinez <dbautista2681@gmail.com>
2021-11-05 12:46:43 -0500

    Added more tiles just need the kitchen to be done

--- src/main/resources/static/js/maps/School.js
+++ src/main/resources/static/js/maps/School.js
@@ -1,4 +1,4 @@
-schoolMap = new MapPath(false, 4564, "/images/maps/school.png", '100%', 25, 16.5, 21, 101);
+schoolMap = new MapPath(false, 4564, "/images/maps/school.png", '100%', 25, 16.5, 121, 151);
 schoolMap.setUpPaths = function() {
      //walls or borders of the map
      schoolMap.switchBlockedRange(3927, 3930);
@@ -11,7 +11,14 @@ schoolMap.setUpPaths = function() {
      schoolMap.switchBlockedRange(4239,4256);
      schoolMap.switchBlockedRangeVert(2413, 18, 83);
      schoolMap.switchBlockedRange(3079,3082);
-     schoolMap.switchBlockedRange(2499,2513)
+     schoolMap.switchBlockedRange(2499,2513);
+     schoolMap.switchBlockedRange(2441, 2455); 
+     schoolMap.switchBlockedRangeVert(2374,7, 83);
+     schoolMap.switchBlockedRangeVert(4008,2, 83);
+     schoolMap.switchBlockedRangeVert(4008,2, 83); 
+
+
+
 
 
 
@@ -25,6 +32,12 @@ schoolMap.setUpPaths = function() {
      //doorways 
      schoolMap.switchBlockedRange(3785, 3788);
      schoolMap.switchBlockedRange(3203, 3208);
+     schoolMap.switchBlockedRangeVert(1769,8, 83);
+     schoolMap.switchBlockedRangeVert(1610,11, 83);
+     schoolMap.switchBlockedRange(1028, 1031);
+
+
+
 
      //green room bottom right 
      schoolMap.switchBlockedRangeVert(2295, 6, 83);
@@ -35,6 +48,25 @@ schoolMap.setUpPaths = function() {
      schoolMap.switchBlockedRange(4218,4222);
      schoolMap.switchBlockedRangeVert(2640, 3, 83);
      schoolMap.switchBlockedRangeVert(3724, 3, 83);
+
+     //bedroom top right 
+     schoolMap.switchBlockedRange(873, 879); 
+     schoolMap.switchBlockedRange(950,955);
+     schoolMap.switchBlockedRange(1463, 1465); 
+     schoolMap.switchBlockedRangeVert(1549,4, 83);
+     schoolMap.switchBlockedRange(1954, 1964); 
+
+     //art room main room 
+     schoolMap.switchBlockedRange(3104, 3120); 
+     schoolMap.switchBlockedRange(3774, 3775);
+     schoolMap.switchBlockedRange(3780, 3781);
+     schoolMap.switchBlocked(3784);
+
+
+
+
+
+
 
 
 
@@ -48,18 +80,52 @@ townMap.beforeGameLoopEvents = function() {
 
 }
 
+townMap.beforeGameLoopEvents = function() {
+    document.querySelectorAll('[class*="npc-"]').forEach(element => {
+        element.remove();
+    });
+    this.colonel = new NPC("b02b81fc-f157-4209-894d-af045be167c7", 30, 145, Array(40).fill("right").concat(Array(10).fill("up")).concat(Array(100).fill("right")).concat(Array(120).fill("down")), 0.5);
+    PopulateNPCSpriteFromServer(this.colonel);
+
+    document.addEventListener("keydown", (e) => {
+        if (e.code === 'Space') {
+            if (this.colonel.characterDialogue.boxIsShowing()) {
+                this.colonel.characterDialogue.hideDialogue();
+            }
+            else {
+                if(detectCollision(this.colonel.character, character)) {
+                    this.colonel.characterDialogue.showDialogue(this.colonel.dialogueTree.World);
+                }
+            }
+        };
+    })
+}
+
 townMap.duringGameLoopEvents = function() {
-      
-    var waterfalldoor = document.querySelectorAll('.mapTileSize')[43];
-    if (detectCollision(character, waterfalldoor)) {
-        currentMap = waterFallMap;
+    this.colonel.placeCharacter();   
+    var schooldoor = document.querySelectorAll('.mapTileSize')[3764];
+    if (detectCollision(character, schooldoor)) {
+        currentMap = townMap;
         currentMap.beforeGameLoopEvents();
         currentMap.drawMap();
         currentMap.setUpPaths();
-        x = currentMap.getStartX();
+        x = 7;
         y = currentMap.getStartY();
     } 
+
+    document.addEventListener("keydown", (e) => {
+        if (e.code === 'Space') {
+            if (this.colonel.characterDialogue.boxIsShowing()) {
+                this.colonel.characterDialogue.hideDialogue();
+            }
+            else {
+                if(detectCollision(this.colonel.character, character)) {
+                    this.colonel.characterDialogue.showDialogue(this.colonel.dialogueTree.HouseTwo[Math.floor(Math.random() * 4)]);
+                }
+            }
+        };
+    })
 }
 
-townMap.afterGameLoopEvents = function() {
+HouseTwoMap.afterGameLoopEvents = function() {
 }
